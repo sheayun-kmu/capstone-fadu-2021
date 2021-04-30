e9eb52c3e3f4ebd6b758a0befa62bb88a07f78e8
Minjeong Kim <kimmj@kookmin.ac.k>
2021-05-01 03:02:13 +0900

    Change XML format

--- plugins/coding_style/coding_style/src/main/java/io/jenkins/plugins/sample/convertToXml.java
+++ plugins/coding_style/coding_style/src/main/java/io/jenkins/plugins/sample/convertToXml.java
@@ -1,4 +1,5 @@
 package io.jenkins.plugins.sample;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,14 +21,14 @@ public class convertToXml  {
 	TransformerHandler transhandler;
 	AttributesImpl attributes;
 
-//	public static void main (String args[]) {
-//		new convertToXml().readFile("./res.txt");
-//	}
+	public static void main (String args[]) {
+		new convertToXml().readFile("./res.txt");
+	}
 
 	public String readFile (String filePath) {
 		try{
 			in = new BufferedReader(new FileReader(filePath));
-			out = new StreamResult("out.xml");
+			out = new StreamResult("/var/jenkins_home/workspace/github_test/out.xml");
 
 			String statement = "";
 			List<String> contents = new ArrayList<String>();
@@ -75,63 +76,75 @@ public class convertToXml  {
 
 		transhandler = tf.newTransformerHandler();
 		Transformer serializer = transhandler.getTransformer();
-		serializer.setOutputProperty(OutputKeys.ENCODING,"ISO-8859-1");
+		serializer.setOutputProperty(OutputKeys.ENCODING,"UTF-8");
 		serializer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 		serializer.setOutputProperty(OutputKeys.INDENT,"yes");
 		transhandler.setResult(out);
 		transhandler.startDocument();
 		attributes = new AttributesImpl();
-		transhandler.startElement("","","TESTCASES",attributes);
+		transhandler.startElement("","","analysisReport",attributes);
 	}
 	
 	public void xmlFileTagStart(String fileName) throws SAXException {
 		attributes.clear();
-		attributes.addAttribute("", "", "file", "", "" + fileName);
-		transhandler.startElement("","","FILETITLE",attributes);
+		//attributes.addAttribute("", "", "class", "", "linked-hash-set");
+		transhandler.startElement("","","elements",attributes);
 		//transhandler.characters(fileName.toCharArray(),0,fileName.length());
 	}
 
 	public void xmlForm (String data[], String code, String position) throws SAXException {
 		attributes.clear();
-		transhandler.startElement("","","CASE",attributes);
+		transhandler.startElement("","","issue",attributes);
 
-		transhandler.startElement("","","FILE",attributes);
-		transhandler.characters(data[0].trim().toCharArray(),0,data[0].length());
-		transhandler.endElement("","","FILE");
+		transhandler.startElement("","","category",attributes);
+		transhandler.characters("category".toCharArray(),0,8);
+		transhandler.endElement("","","category");
 		
-		transhandler.startElement("","","LINE",attributes);
-		transhandler.characters(data[1].trim().toCharArray(),0,data[1].length());
-		transhandler.endElement("","","LINE");
+		transhandler.startElement("","","type",attributes);
+		transhandler.characters("type".toCharArray(),0,4);
+		transhandler.endElement("","","type");
 		
-		transhandler.startElement("","","RULE",attributes);
-		transhandler.characters(data[2].trim().toCharArray(),0,data[2].length());
-		transhandler.endElement("","","RULE");
-		
-		transhandler.startElement("","","SEVERITY",attributes);
+		transhandler.startElement("","","severity",attributes);
 		transhandler.characters(data[3].toCharArray(),0,data[3].length());
-		transhandler.endElement("","","SEVERITY");
+		transhandler.endElement("","","severity");
 		
-		transhandler.startElement("","","MESSAGE",attributes);
+		transhandler.startElement("","","message",attributes);
 		transhandler.characters(data[4].toCharArray(),0,data[4].length());
-		transhandler.endElement("","","MESSAGE");
+		transhandler.endElement("","","message");
 		
-		transhandler.startElement("","","CODE",attributes);
-		transhandler.characters(code.toCharArray(),0,code.length());
-		transhandler.endElement("","","CODE");
+		transhandler.startElement("","","lineStart",attributes);
+		transhandler.characters(data[1].trim().toCharArray(),0,data[1].length());
+		transhandler.endElement("","","lineStart");
+		
+		transhandler.startElement("","","lineEnd",attributes);
+		transhandler.characters(data[1].trim().toCharArray(),0,data[1].length());
+		transhandler.endElement("","","lineEnd");
+		
+		transhandler.startElement("","","columnStart",attributes);
+		transhandler.characters(data[2].trim().toCharArray(),0,data[2].length());
+		transhandler.endElement("","","columnStart");
+		
+		transhandler.startElement("","","columnEnd",attributes);
+		transhandler.characters(data[2].trim().toCharArray(),0,data[2].length());
+		transhandler.endElement("","","columnEnd");
+		
+//		transhandler.startElement("","","CODE",attributes);
+//		transhandler.characters(code.toCharArray(),0,code.length());
+//		transhandler.endElement("","","CODE");
+//
+//		transhandler.startElement("","","POSITION",attributes);
+//		transhandler.characters(position.toCharArray(),0,position.length());
+//		transhandler.endElement("","","POSITION");
 
-		transhandler.startElement("","","POSITION",attributes);
-		transhandler.characters(position.toCharArray(),0,position.length());
-		transhandler.endElement("","","POSITION");
-
-		transhandler.endElement("","","CASE");
+		transhandler.endElement("","","issue");
 	}
 	
 	public void xmlFileTagEnd() throws SAXException {
-		transhandler.endElement("","","FILETITLE");
+		transhandler.endElement("","","elements");
 	}
 
 	public void closeXML() throws SAXException {
-		transhandler.endElement("","","TESTCASES");
+		transhandler.endElement("","","analysisReport");
 		transhandler.endDocument();  
 	}
 }
